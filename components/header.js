ff37ec6b3a7fe008a73eb59cbc11f9c5be3e226c
Pooh <sophon@punchup.world>
2021-01-27 22:15:17 +0700

    Set default font in global css

--- components/header.js
+++ components/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function header() {
   return (
-    <div id="Header" className=" bg-white-default" style={{ height: "60px" }}>
+    <div id="header" className="sticky z-10 bg-white-default" style={{ height: "60px" }}>
       <Link href="/">
         <a className=" text-pink-default">Home</a>
       </Link>
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,5 +2,37 @@ import styles from "../styles/Dashboard.module.css";
 import Layout from "../layouts/layout";
 
 export default function Dashboard() {
-  return <Layout>dashboard</Layout>;
+  const category = [
+    { น้ำท่วมถนน: "#1570FF" },
+    { พื้นที่สีเขียว: "#00C853" },
+    { มลพิษในคลอง: "#FF9E0D" },
+    { ปริมาณขยะมูลฝอย: "#DF3A6B" },
+    { ฝุ่นควันเกินมาตรฐาน: "#476A8B" },
+  ];
+  return (
+    <Layout>
+      <div
+        className="absolute top-0 bottom-0 left-0 right-0 flex flex-row bg-black-default text-white-default"
+        id="dashboard-wrapper"
+        style={{ paddingTop: "60px" }}
+      >
+        <div className="flex flex-col flex-1" id="intro-dashboard-left">
+          <div className="flex items-center justify-center flex-1">
+            <span>
+              <p className="d5">หรืออีกวิธีที่สามารถมีส่วนร่วมได้ก็คือ</p>
+              <p className="d3">
+                ร่วมกันสำรวจสถานการณ์ ปัญหาด้านต่างๆ ของกรุงเทพฯ ในรอบ 8
+                ปีที่ผ่านมา
+              </p>
+              <p className="d5">ข้อมูลย้อนหลัง พ.ศ. 2555-2562</p>
+            </span>
+          </div>
+          <div className="flex-1">bottom</div>
+        </div>
+        <div className="flex-1" id="intro-dashboard-right">
+          2
+        </div>
+      </div>
+    </Layout>
+  );
 }
