0b4d1c4893702063fa3ea861140652461245f960
Pooh <sophon@punchup.world>
2021-02-25 17:52:51 +0700

    Fixed default

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 const withImages = require('next-images');
 module.exports = withImages({
-  distDir: 'build',
-  webpack(config) {
+  // distDir: 'build',
+  target: 'serverless',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/dashboard': { page: '/dashboard' },
+      '/intro-dashboard': { page: '/intro-dashboard' },
+    };
+  },
+  webpack(config, options) {
     return config;
   },
 });
