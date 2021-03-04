6178103631c63c82356133b200034c560ed4b7c3
Krit Tantisangarun <>
2021-03-05 00:37:54 +0700

    Disable Server-Side Rendering

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,20 @@ import "styles/dashboard.scss";
 import "styles/home.scss";
 import "swiper/swiper.scss";
 
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  );
 }
 
 export default MyApp;
